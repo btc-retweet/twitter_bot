5db60a79102687c7d8412b3aeed823475d92288c
Bryan Hohs <bryan@hohs.us>
2022-06-01 10:53:41 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,44 @@
+#
+# tweepy bot forked from github
+#
+import tweepy
+import logging
+from credentials import *
+
+logging.basicConfig(format='%(levelname)s [%(asctime)s] %(message)s', datefmt='%r', level=logging.INFO)
+logger = logging.getLogger()
+
+# keywords to search for
+search_keywords = "%23bitcoin"
+
+# time to wait between searching tweets in seconds 
+search_delay = 15 
+
+# time to wait before processing more tweets
+process_delay = 180
+
+# 'recent', 'popular', or 'mixed'
+result_type = 'popular'
+
+# number of tweets you want to fetch from twitter 
+number_of_tweets = 60 
+
+# like tweets 
+like_tweets = True
+
+# retweet tweets
+retweet_tweets = True
+
+def create_api():
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error('Authentication Error', exc_info=True)
+        raise e
+    logger.info(f"Authentication OK. Connected to @{api.me().screen_name}")
+
+    return api
---
+++ twitter-bot.py
@@ -0,0 +1,86 @@
+import os
+import logging
+import tweepy
+from time import sleep
+from config import *
+
+__author__ = 'Bryan Hohs'
+__version__ = '2.0.0'
+__maintainer__ = 'Bryan Hohs'
+__email__ = 'bryan@hohs.us'
+__status__ = 'Development'
+
+logging.basicConfig(format='%(levelname)s [%(asctime)s] %(message)s', datefmt='%r', level=logging.INFO)
+logger = logging.getLogger()
+
+def banner_text():
+    logger.info("-------------------------------------------------")
+    logger.info("                                                 ")
+    logger.info(" _______ ________ _______ _______ ______ ___ ___ ")
+    logger.info("|_     _|  |  |  |    ___|    ___|   __ \   |   |")
+    logger.info("  |   | |  |  |  |    ___|    ___|    __/\     / ")
+    logger.info("  |___| |________|_______|_______|___|    |___|  ")
+    logger.info("            ______ _______ _______               ")
+    logger.info("           |   __ \       |_     _|              ")
+    logger.info("           |   __ <   -   | |   |                ")
+    logger.info("           |______/_______| |___|                ")
+    logger.info("                                                 ")
+    logger.info("-------------------------------------------------")
+
+def initialize_api():
+    api = create_api()
+    return api
+
+def get_tweets(api):
+    tweets = tweepy.Cursor(api.search,
+                    q=search_keywords + " -filter:retweets",
+                    count=number_of_tweets,
+                    result_type=result_type,
+                    monitor_rate_limit=True,
+                    wait_on_rate_limit=True,
+                    lang="en").items()
+    return tweets
+
+def process_tweets(api, tweets):
+    for tweet in tweets:
+        tweet = api.get_status(tweet.id)
+        logger.info(f"🔎 Searching {result_type} tweets: {tweet.text[:25] + (tweet.text[25:] and '..')}")
+
+        if tweet.user.id != api.me().id or tweet.in_reply_to_status_id is not None:
+
+            if retweet_tweets:
+                if not tweet.retweeted:
+                    try:
+                        tweet.retweet()
+                        logger.info(f"👍 We have a tweet from @{tweet.user.screen_name}, retweeting now!")
+                    except Exception as e:
+                        logger.error("Error on retweet", exc_info=True)
+                        raise e
+                else:
+                    logger.info(f"❌ The tweet from @{tweet.user.screen_name} has already been retweeted.")
+
+            if like_tweets:
+                if not tweet.favorited:
+                    try:
+                        tweet.favorite()
+                        logger.info(f"👍 We have a tweet from @{tweet.user.screen_name}, liking now!")
+                        logger.info(f"⌛ Waiting {process_delay} seconds so we don't flood.")
+                        sleep(process_delay)
+                    except Exception as e:
+                        logger.error("Error on favorite", exc_info=True)
+                        raise e
+                else:
+                    logger.info(f"❌ The tweet from @{tweet.user.screen_name} has already been liked.")
+
+        sleep(search_delay)
+
+def exit_text():
+    logger.info("Goodbye!")
+
+if __name__ == "__main__":
+    welcome = banner_text()
+    api = initialize_api()
+    tweets = get_tweets(api)
+    process_tweets(api, tweets)
+
+exit_text()
